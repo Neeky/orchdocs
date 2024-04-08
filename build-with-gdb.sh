84d3910b98bf08085593f41f8b1d47cdf8a2464b
Neeky <1721900707@qq.com>
2024-04-08 14:29:10 +0800

    ✨feature: 改进打包脚本

--- build-with-gdb.sh
+++ build-with-gdb.sh
@@ -1,1 +1,15 @@
 go build -gcflags="all=-N -l " -o bin/orchestrator go/cmd/orchestrator/main.go
+
+# 创建输出目录
+rm -rf orchestrator
+rm -rf orchestrator-3.2.6.tar.gz
+
+mkdir orchestrator
+cp -rf resources orchestrator/
+mkdir orchestrator/bin/
+cp bin/orchestrator orchestrator/bin/
+
+
+tar -czvf orchestrator-3.2.6.tar.gz orchestrator
+
+
